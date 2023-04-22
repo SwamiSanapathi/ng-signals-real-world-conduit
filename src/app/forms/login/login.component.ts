6bf36225f1dd050402b211f054c12716c0610b64
AnilSanapathi <anilsanapathi9@gmail.com>
2023-04-22 20:36:09 +0530

    add signup routing

--- src/app/forms/login/login.component.ts
+++ src/app/forms/login/login.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 import { NewUser } from 'src/app/models/new-user';
 import { AuthService } from 'src/app/services/auth-service';
 @Component({
     standalone: true,
-    imports: [FormsModule, NgFor, NgIf, CommonModule],
+    imports: [FormsModule, NgFor, NgIf, CommonModule, RouterLink],
     templateUrl: './login.component.html',
 })
 export default class LoginComponent implements OnInit {
